580e46c9dd7c33d99e29a00b59e2641c895dcdab
sgbaotran <sttr2130@gmail.com>
2025-11-23 22:38:16 -0500

    Initlize model for hunter

--- room.c
+++ room.c
@@ -1,14 +1,6 @@
 #include "room.h"
 
-/**
- * COMPLETE: Initializes a Room structure with the given name and exit flag.
- *
- * @param room     Pointer to the Room struct to initialize.
- * @param name     Null-terminated string representing the room's name.
- * @param is_exit  Boolean indicating whether the room is an exit.
- *
- * @return         Nothing
- */
+
 void room_init(struct Room *room, const char *name, bool is_exit)
 {
     room->connection_count = 0;
